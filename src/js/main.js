cf10a9803b64ce2fb6737870bb59c05ba447a90e
Anzhelika <samchukanzhelika@yandex.ru>
2021-11-24 01:29:59 +0700

    Создание sprint4. Подключение jquery.Slider-услуги

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$('.ourworks').slick({
+		prevArrow: '.btn-prev',
+		nextArrow: '.btn-next',
+		slidesToShow: 4,
+		infinite: true //бесконечный слайдер
+	});
+
+
+
+});
