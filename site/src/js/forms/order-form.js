c5be68ce4dd0bf364c76c6c1157c5f249ebf4bcd
Anzhelika <samchukanzhelika@yandex.ru>
2021-12-06 21:57:28 +0700

    Запрос к серверу

--- site/src/js/forms/order-form.js
+++ site/src/js/forms/order-form.js
@@ -1,6 +1,6 @@
 export class OrderForm {
 
-	changedForm(event, loader, success, form) {
+	async changedForm(event, loader, success, form) {
 		const formData = Array
 			.from(event.target.elements)
 			.filter(el => el.name)
@@ -11,12 +11,18 @@ export class OrderForm {
 
 		console.log(formData);
 
-		/*fetch(`http://localhost:3001/api/customers?fullName`)
-		//информация об ответе, статус, заголовки и данные и распаковываем методом json
-		.then(response => response.json());//ворачивается данные*/
+			/*response = await fetch('http://localhost:3001/api/customers?fullName', {
+			method: 'POST',
+			body: formData,
+			headers: {
+				'Content-Type': 'multipart/form-data'
+			}
+		});
 
-		loader.style.display = 'block';
-		this._sendRequest(() => {
+		
+			if (response.ok) {*/
+			loader.style.display = 'block';
+			this._sendRequest(() => {
 			//после того как получили ответ блокируем лоадер
 			loader.style.display = 'none';
 			form.reset(); //сброс формы
@@ -24,8 +30,11 @@ export class OrderForm {
 			this._timeout(() => {
 				success.style.display = 'none';
 			}, 3000);
-
 		});
+		/*} else {
+			alert("Ошибка HTTP: " + response.status);
+		 }*/
+
 	}
 
 	_timeout(callback, time) {
