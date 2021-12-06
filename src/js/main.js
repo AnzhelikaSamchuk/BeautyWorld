44395a5ed84fc11b7e43f2b8044187e305e74fd2
AnzhelikaSamchuk <92257930+AnzhelikaSamchuk@users.noreply.github.com>
2021-12-06 19:38:15 +0700

    Sprint4 (#4)
    * Создала .html
    
    * Добавила header, main, footer
    
    * Изменение header, main, footer
    
    * Начала делать стилизацию header
    
    * Добавление стилей
    
    * Стиль у девушки
    
    * Шапка готова
    
    * Перешла на стилизацию main
    
    * Добавила блок на видео
    
    * Секция о салоне
    
    * О салоне готов
    
    * Секция "Преимущества" выполнена
    
    * Секция "Преимущества" готова
    
    * Готово
    
    * requested changes
    
    * Все замечания исправлены!
    
    * Создание 2 ветки (sprint2)
    
    * Изменения в benefit как на лекции
    
    * Блок services готов
    
    * Написана конфигурация
    
    * внесение изменений
    
    * Подправила футер и еще небольшие изменения
    
    * Секция мастера готова
    
    * Разде "Услуги и цены" готово
    
    * Услуги и цены поправила, были ошибки
    
    * Рефакторинг кода
    
    * sprint2 выполнен
    
    * Адаптивность шапки исправлена
    
    * Адаптивность всех блоков, испр. своих же недочетов
    
    * Обновление до main
    
    * Секция "contacts" выполнена
    
    * Исправила все замечания кроме удаления ст.вн.ресур
    
    * изменения
    
    * Блок наши работы
    
    * Доработка в секции "Мастер" при наведении
    
    * Готова секция "Наши работы"
    
    * Небольшие изм. (margin для contacts и masters)
    
    * sprint3 готов
    
    * Окончательный вариант sprint3 без ст.вн.ресурсов
    
    * Добавление в readme
    
    * add readme
    
    * add readme
    
    * Создание sprint4. Подключение jquery.Slider-услуги
    
    * Секция наши работы готова
    
    * Мобильное мени и плавность до ссылок выполнено
    
    * Секция услуги и цены готова
    
    * Подготовка формы и модальное окно с видео готово
    
    * Доработки по замечаниям: секция мастеров готова
    
    * Доработки по замечаниям: секция наши работы готова
    
    * sprint4 готов
    
    * конфликты
    
    * Доработка небольших замечаний от 01.12 лек

---
+++ src/js/main.js
@@ -0,0 +1,106 @@
+import { TabsManager } from './tabs.js';
+
+$(document).ready(function () {
+	/*Мобильное меню*/
+	humb.addEventListener('click', () => {
+		mobileMenu.classList.add('visible');
+	});
+
+	closeHumb.addEventListener('click', () => {
+		mobileMenu.classList.remove('visible');
+	});
+
+	const navigationsHumb = document.querySelectorAll('#navigation-humb');
+	for (let link of navigationsHumb) {
+		link.addEventListener('click', event => {
+			console.log('click');
+			mobileMenu.classList.remove('visible');
+		})
+	}
+
+	/*Плавный скролл*/
+	const navigation = document.querySelector('.navigation');
+
+	document.addEventListener('scroll', event => {
+		console.log(event.target.scrollTop)
+	});
+
+	const smoothScrollLinks = document.querySelectorAll('.smooth-scroll');
+
+	for (let link of smoothScrollLinks) {
+		link.addEventListener('click', event => {
+			event.preventDefault();
+
+			const target = event.target;
+			const elementToScroll = document.querySelector(target.getAttribute('href'));
+			elementToScroll.scrollIntoView({ behavior: 'smooth', block: 'start' });
+		});
+	}
+
+	/*Наши работы*/
+	$('.ourworks').on('init reInit', function (e, slick) {
+		if (slick.slideCount <= slick.options.slidesToShow) {
+			slick.slickAdd(slick.$slides.clone())
+		}
+	})
+
+	$('.ourworks').slick({
+		prevArrow: '.btn-prev',
+		nextArrow: '.btn-next',
+		slidesToShow: 4,
+		infinite: true, //бесконечный слайдер
+		slidesToScroll: 1,
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 3,
+					infinite: true,
+					slidesToScroll: 1
+				}
+			},
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 2,
+					infinite: true,
+					slidesToScroll: 1
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 1,
+					infinite: true,
+					slidesToScroll: 1
+				}
+			}
+		]
+	});
+
+	/*pricetabs*/
+	const tabs = new TabsManager(document.getElementById('pricetabs'));
+
+	/*Форма - inputmask*/
+	$('#phone').inputmask({"mask": "+7 (999) 999-9999"});
+
+	/*Форма*/
+	const form = document.getElementById('form');
+
+	form.addEventListener('submit', event => {
+		event.preventDefault();
+
+		const formData = Array
+			.from(event.target.elements)
+			.filter(el => el.name)
+			.map(el => {
+			const {value, name} = el;
+			return { [name] : value};
+			});
+
+			console.log(formData);
+	});
+
+
+
+});
