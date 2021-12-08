2382dd7f34beed6cc783ddd73e1848f18631cd10
Anzhelika <samchukanzhelika@yandex.ru>
2021-12-08 23:35:35 +0700

    sprint5

--- site/src/js/main.js
+++ site/src/js/main.js
@@ -1,4 +1,6 @@
+import $ from 'jquery';//глобальные импорты выше
 import { TabsManager } from './tabs.js';
+import { OrderForm } from './forms/order-form.js';
 
 $(document).ready(function () {
 	/*Мобильное меню*/
@@ -10,21 +12,16 @@ $(document).ready(function () {
 		mobileMenu.classList.remove('visible');
 	});
 
-	const navigationsHumb = document.querySelectorAll('#navigation-humb');
-	for (let link of navigationsHumb) {
-		link.addEventListener('click', event => {
-			console.log('click');
+	const navigationsHumb = document.getElementById('navigation-humb');
+	navigationsHumb.addEventListener('click', event => {
+		event.preventDefault();
+		const isTabLink = event.target.classList.contains('smooth-scroll');
+
+		if (isTabLink)
 			mobileMenu.classList.remove('visible');
-		})
-	}
-
-	/*Плавный скролл*/
-	const navigation = document.querySelector('.navigation');
-
-	document.addEventListener('scroll', event => {
-		console.log(event.target.scrollTop)
 	});
 
+	/*Плавный скролл*/
 	const smoothScrollLinks = document.querySelectorAll('.smooth-scroll');
 
 	for (let link of smoothScrollLinks) {
@@ -82,23 +79,59 @@ $(document).ready(function () {
 	const tabs = new TabsManager(document.getElementById('pricetabs'));
 
 	/*Форма - inputmask*/
-	$('#phone').inputmask({"mask": "+7 (999) 999-9999"});
+	//$('#phone').inputmask({"mask": "+7 (999) 999-9999"});
+
+	$("#form-contact").validate({
+		rules: {
+			name: {
+				required: true,
+				minlength: 2
+			},
+			phone: {
+				required: true,
+				maxlength: 11,
+				minlength: 11
+			}
+		},
+		messages: {
+			name: {
+				required: "Поле 'Ваше Имя' обязательно к заполнению",
+				minlength: "Введите не менее 2-х символов в поле 'Ваше Имя'"
+			},
+			phone: {
+				required: "Поле 'телефон' обязательно к заполнению",
+				minlength: "Введите не менее 11-х символов в поле 'Телефон'",
+				maxlength: "Введите не более 11-х символов в поле 'Телефон'"
+			}
+		}
+	});
 
 	/*Форма*/
-	const form = document.getElementById('form');
+	const formConntact = document.getElementById('form-contact');
+	const loaderContact = document.getElementById('loader-contact');
+	const successContact = document.getElementById('success-contact');
 
-	form.addEventListener('submit', event => {
+	const formExtended = document.getElementById('form-extended');
+	const loaderExtended = document.getElementById('loader-extended');
+	const successExtended = document.getElementById('success-extended');
+
+	const form = new OrderForm();
+
+	formConntact.addEventListener('submit', event => {
 		event.preventDefault();
 
-		const formData = Array
-			.from(event.target.elements)
-			.filter(el => el.name)
-			.map(el => {
-			const {value, name} = el;
-			return { [name] : value};
-			});
+		if ($("#form-contact").valid()) {
+			form.changedForm(event, loaderContact, successContact, formConntact);
+		}
+	});
 
-			console.log(formData);
+	formExtended.addEventListener('submit', event => {
+		event.preventDefault();
+
+		if ($("#form-extended").valid()) {
+			form.changedForm(event, loaderExtended, successExtended, formExtended);
+			// ! не смогла закрыть модалку, после нажатия на кнопку
+		}
 	});
 
 
